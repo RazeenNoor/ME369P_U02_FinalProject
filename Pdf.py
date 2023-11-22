d520d22c53b1e731083ea40d8bf5214893c51500
JR1vas <143118611+JR1vas@users.noreply.github.com>
2023-11-21 18:31:37 -0600

    formats and line wrapping

--- Pdf.py
+++ Pdf.py
@@ -1,5 +1,3 @@
-# PDF.py
-
 import Scrape
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -9,54 +7,109 @@ from datetime import datetime
 class BuildPDF:
     def __init__(self, city):
         self.city = city.lower()
-        if self.city == "austin":
-            self.generate_pdf('Austin.pdf')
-        elif self.city == "boston":
-            self.generate_pdf('Boston.pdf')
-        elif self.city == "chicago":
-            self.generate_pdf('Chicago.pdf')
+        filename = f"{self.city.capitalize()}.pdf"
+        if self.city in ["austin", "boston", "chicago"]:
+            self.generate_pdf(filename)
         else:
             print(f"City '{city}' is not supported.")
 
     def generate_pdf(self, filename):
         city_instance = Scrape.Cities(self.city)
         current_date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # Generate PDF content using the city_instance
-        pdf_content = f"Information for {self.city.capitalize()} as of {current_date_time}\n\n"
-        pdf_content += f"Weather: {city_instance.weather()['temp']}, {city_instance.weather()['precip']}, {city_instance.weather()['conditions']}\n\n"
-        pdf_content += "Current News:\n"
+        current_weather, forecast = city_instance.weather()
+        
+        # Title and Current Weather
+        pdf_content = (
+                        f"<T>Information for {self.city.capitalize()}<TT>\nas of {current_date_time}\n\n" #Title
+                       
+                        f"<H>Current Weather:<HH>\n" #Heading
+                        f"-Temperature: {current_weather['temp']}\n"
+                        f"-Precipitation: {current_weather['precip']}\n"
+                        f"-Current Conditions:\n{current_weather['conditions']}\n"
+                       )
+
+        # 3-Day Forecast 
+        pdf_content += "\n<H>3-Day Forecast:<HH>\n" #Heading
+        for date, details in forecast.items():
+            pdf_content += (f"- {date}:\n"
+                            f"     High/Low: {details[0]}\n"
+                            f"     Chance of Rain: {details[1]}\n"
+                            f"     Description: {details[2]}\n"
+                            )
+          
+        # Current News
+        pdf_content += "\n<H>Current News:<HH>\n" #Heading
         for headline in city_instance.news():
             pdf_content += f"- {headline}\n"
-        pdf_content += "\nEvents:\n"
+        
+        # Events
+        pdf_content += "\n<H>Events:<HH>\n" #Heading
         for event, (date, time) in city_instance.events().items():
             pdf_content += f"- {event} on {date} at {time}\n"
-        pdf_content += "\nLandmarks:\n"
+        
+        # Landmarks
+        pdf_content += "\n<H>Landmarks:<HH>\n" #Heading
         for landmark, img_url in city_instance.landmarks().items():
-            pdf_content += f"- {landmark}\n"
+            pdf_content += f"- {landmark} {img_url}\n"
 
         # formatting
         c = canvas.Canvas(filename, pagesize=letter)
         width, height = letter
-
-        # font and size
-        c.setFont("Helvetica", 12)
-
-        # margins
         margin = 50
-        # usable_width = width - 2 * margin
-        # usable_height = height - 2 * margin
+        
+        #Fonts
+        Tfont,Tsize="Helvetica-Bold",20
+        Hfont,Hsize="Courier-Bold",16
+        tfont,tsize="Helvetica",12
+        
+        #Font colors
+        Hcolor = (0, 0, 1)  # RGB (0-1)
+        Tcolor = (1, 0, 0)
+        tcolor = (0, 0, 0)
 
-        # Draw text with proper line spacing
+        # line spacing
         lines = pdf_content.split('\n')
-        line_height = 14  # Adjust as needed
+        line_height = 14 
+        max_line_width = width - 2 * margin  # Maximum width for a line
         current_height = height - margin
+
         for line in lines:
-            if current_height - line_height < margin:
-                # Start a new page if the text exceeds the page height
-                c.showPage()
-                current_height = height - margin
-            c.drawString(margin, current_height, line)
-            current_height -= line_height
+            if "<T>" in line:
+                # Title line
+                c.setFillColor(Tcolor)
+                c.setFont(Tfont, Tsize)
+                title = line.replace("<T>", "").replace("<TT>", "")
+                c.drawString(margin, current_height, title)
+                current_height -= line_height
+            elif "<H>" in line:
+                # Heading line
+                c.setFillColor(Hcolor)
+                c.setFont(Hfont, Hsize)
+                heading = line.replace("<H>", "").replace("<HH>", "")
+                c.drawString(margin, current_height, heading)
+                current_height -= line_height
+            else:
+                c.setFillColor(tcolor)
+                c.setFont(tfont, tsize)
+                
+                # Wrapping
+                # Split the line into words
+                words = line.split()
+                current_line = ""
+
+                for word in words:
+                    # Check if next word exceeds max_line_width
+                    if c.stringWidth(current_line + word, tfont, tsize) < max_line_width:
+                        current_line += word + " "
+                    else:
+                        # start a new line
+                        c.drawString(margin, current_height, current_line.strip())
+                        current_height -= line_height
+                        current_line = word + " "
+
+                # Draw rest of line
+                c.drawString(margin, current_height, current_line.strip())
+                current_height -= line_height
 
         c.save()
 
